dcf5f13ada4725d5011e99694f5ba1a276d958e0
elevenfang <leidongfang@yeah.net>
2017-09-07 23:04:31 +0800

    wrong repository

--- blog/BlogWebSite/src/main/java/com/elevenfang/blog/constant/BlogConstant.java
+++
@@ -1,9 +0,0 @@
-package com.elevenfang.blog.constant;
-
-public class BlogConstant {
-
-	public static final String BLOG_MONGO_DB_NAME = "WorkingOverTimeManagement";
-
-	private BlogConstant() {
-	}
-}
